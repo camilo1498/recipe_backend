24f0baa583227be8e05d3d959bb6e68e8fa40425
camilo1498 <davidcamilo96000@gmail.com>
2023-04-19 15:47:46 -0500

    no message

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,11 @@ const recipeRoutes = require('./recipe_routes')
 
 /// init routes
 module.exports = (app) => {
+    app.get('*', (req, res) => { res.status(404).json({ 'success': false, 'message': 'api not found' }) })
+    app.post('*', (req, res) => { res.status(404).json({ 'success': false, 'message': 'api not found' }) })
+    app.put('*', (req, res) => { res.status(404).json({ 'success': false, 'message': 'api not found' }) })
+    app.delete('*', (req, res) => { res.status(404).json({ 'success': false, 'message': 'api not found' }) })
+    app.patch('*', (req, res) => { res.status(404).json({ 'success': false, 'message': 'api not found' }) })
     userroutes(app)
     roleRoutes(app)
     recipeRoutes(app)
